e5976a3e6b7c4997f783066452b79c642cbd601a
Austin Middleton <xuset1@gmail.com>
2015-08-23 15:26:21 -0500

    Added support for cross domain resource sharing
    This is needed for any web clients to access the api, because
    browser access control policies will prevent them otherwise.
    
    NOTE: The "Access-Control-Allow-Origin" will need to be changed
    later once there is a domain name for server hosting the web
    client.

--- rest_handler.py
+++ rest_handler.py
@@ -12,6 +12,14 @@ from common import Errors, TransactionStatus
 app = Flask(__name__)
 app.debug = True
 
+@app.after_request
+def after_request(response):
+    #TODO - Important Security - replace '*' with name of the server hosting the delegator web client
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    return response
+
 @app.route('/')
 def index():
     return "GatorRestService is up and running!"
