856afddcd417503ab88f65bf1d868a4a3eb436d1
Austin Middleton <xuset1@gmail.com>
2015-08-23 15:32:40 -0500

    Marked as executable and added cli parser
    Just so it is easier to start and so the default port can be easily
    changed since port 80 requires root.

--- rest_handler.py
+++ rest_handler.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 from flask import Flask, request
 
 import time
+import argparse
 import jsonpickle
 
 import models
@@ -219,4 +222,9 @@ def verify_dict_contains_keys(dic, keys):
     return True
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=80, debug=True, threaded=True)
+    parser = argparse.ArgumentParser(description="Starts the api server")
+    parser.add_argument("--port", "-bp", dest="port", type=int, default=80, help="The port to bind to")
+    parser.add_argument("--host", "-bh", dest="host", default="0.0.0.0", help="The hostname to bind to")
+
+    args = parser.parse_args()
+    app.run(host=args.host, port=args.port, debug=True, threaded=True)
