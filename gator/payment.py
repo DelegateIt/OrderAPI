8707aa4a7454c2e883c22315a842fcecba51617f
Austin Middleton <xuset1@gmail.com>
2015-09-05 14:20:52 -0500

    Moved the html into seperate templates
    Also handled the specific exceptions and stripe card exceptions
    by showing a nice little error page

--- gator/payment.py
+++ gator/payment.py
@@ -1,64 +1,46 @@
+import logging
 import stripe
-from flask import request
+from flask import request, render_template
 
 import gator.models
 from gator import app
 
 stripe.api_key = "sk_test_WYJIBAm8Ut2kMBI2G6qfEbAH"
 
-@app.route('/core/payment/uiform/<transaction_uuid>', methods=['GET'])
-def gen_ui_form(transaction_uuid):
-    if not gator.models.transactions.has_item(uuid=transaction_uuid, consistent=True):
-        raise Exception("Transaction does not exist")
-    transaction = gator.models.transactions.get_item(uuid=transaction_uuid, consistent=True)._data
-    if "receipt" not in transaction:
-        raise Exception("Transaction has not been finalized")
-    if transaction['receipt']['paid_for']:
-        raise Exception("Transaction has already been paid for")
-    amount = 0
-    for item in transaction['receipt']['items']:
-        amount += item['cents']
+class PaymentException(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
 
-    #TODO display 'already paid for' page if transaction is paid for
-    #TODO move this to seperate file and make it look pretty
-    page = '''
-        <form action="/core/payment/uicharge/{uuid}" method="POST">
-            <script
-                src="https://checkout.stripe.com/checkout.js" class="stripe-button"
-                data-key="pk_test_ZoK03rN4pxc2hfeYTzjByrdV"
-                data-name="DelegateIt"
-                data-description="Your personal concierge service"
-                data-amount="{amount}"
-                data-locale="auto">
-            </script>
-            <input type="hidden" name="amount" value="{amount}">
-        </form>
-    '''.format(uuid=transaction_uuid, amount=amount)
-    return page
-
-@app.route('/core/payment/uicharge/<transaction_uuid>', methods=['POST'])
-def ui_charge(transaction_uuid):
+def get_chargeable_transaction(transaction_uuid):
     if not gator.models.transactions.has_item(uuid=transaction_uuid, consistent=True):
-        raise Exception("Transaction does not exist")
-    #TODO email receipt
-    #TODO handle all stripe errors
-    email = request.form['stripeEmail']
-    amount = int(request.form['amount'])
+        raise PaymentException("Transaction does not exist")
     db_transaction = gator.models.transactions.get_item(uuid=transaction_uuid, consistent=True)
-    db_customer = gator.models.customers.get_item(uuid=db_transaction._data['customer_uuid'])
     if "receipt" not in db_transaction._data:
-        raise Exception("Transaction has not been finalized")
+        raise PaymentException("The transaction has not been finalized")
     if db_transaction._data['receipt']['paid_for']:
-        raise Exception("Transaction has already been paid for")
+        raise PaymentException("The transaction has already been paid for")
+    return db_transaction
+
+def calculate_total(receipt):
+    amount = 0
+    for item in receipt['items']:
+        amount += item['cents']
+    return amount
+
+def charge_transaction(transaction_uuid, stripe_token, email):
+    #TODO email receipt
+    #TODO store charge id
+    db_transaction = get_chargeable_transaction(transaction_uuid)
+    db_customer = gator.models.customers.get_item(uuid=db_transaction._data['customer_uuid'])
 
     stripe_customer = stripe.Customer.create(
-        source=request.form['stripeToken'],
+        source=stripe_token,
         description="Paid via link",
         email=email
     )
 
     stripe.Charge.create(
-        amount=amount, #in cents
+        amount=calculate_total(db_transaction['receipt']), #in cents
         currency="usd",
         customer=stripe_customer.id
     )
@@ -68,5 +50,27 @@ def ui_charge(transaction_uuid):
     db_customer.save()
     db_transaction._data['receipt']['paid_for'] = True
     db_transaction.save()
-    return "You were successfully charged ${}".format(amount / 100.0)
+
+@app.route('/core/payment/uiform/<transaction_uuid>', methods=['GET'])
+def ui_form(transaction_uuid):
+    try:
+        transaction = get_chargeable_transaction(transaction_uuid)
+    except PaymentException as e:
+        logging.exception(e)
+        return render_template('payment-error.html', message=str(e))
+    amount = calculate_total(transaction['receipt'])
+    return render_template('payment.html', uuid=transaction_uuid, amount=amount, total=float(amount)/100.0, items=transaction['receipt']['items'])
+
+@app.route('/core/payment/uicharge/<transaction_uuid>', methods=['POST'])
+def ui_charge(transaction_uuid):
+    email = request.form['stripeEmail']
+    token = request.form['stripeToken']
+    try:
+        charge_transaction(transaction_uuid, token, email)
+    except stripe.error.CardError as e:
+        return render_template('payment-error.html', message=str(e))
+    except PaymentException as e:
+        logging.exception(e)
+        return render_template('payment-error.html', message=str(e))
+    return render_template('payment-success.html')
 
