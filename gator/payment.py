40a35c84cad839061f0d5ba7a0932f5c36f68501
Austin Middleton <xuset1@gmail.com>
2015-09-16 16:03:30 -0500

    Utilizing redirects for better ui experience
    Once the transaction has been charged, the user is redirected instead
    of displaying the template at the current url. This prevents the
    refresh dialog box from opening due to POST requests.
    Also, refreshing the url no longer display an error if the transaction
    has already been paid.
    Also changed the url schema

--- gator/payment.py
+++ gator/payment.py
@@ -1,6 +1,7 @@
 import logging
+import urllib
 import stripe
-from flask import request, render_template
+from flask import request, render_template, redirect
 
 import gator.models
 from gator import app
@@ -17,8 +18,6 @@ def get_chargeable_transaction(transaction_uuid):
     db_transaction = gator.models.transactions.get_item(uuid=transaction_uuid, consistent=True)
     if "receipt" not in db_transaction:
         raise PaymentException("The transaction has not been finalized")
-    if "stripe_charge_id" in db_transaction['receipt']:
-        raise PaymentException("The transaction has already been paid for")
     return db_transaction
 
 def calculate_total(receipt):
@@ -30,6 +29,8 @@ def calculate_total(receipt):
 def charge_transaction(transaction_uuid, stripe_token, email):
     #TODO email receipt
     db_transaction = get_chargeable_transaction(transaction_uuid)
+    if "stripe_charge_id" in db_transaction['receipt']:
+        return #It has already been paid
     db_customer = gator.models.customers.get_item(uuid=db_transaction['customer_uuid'])
 
     stripe_customer = stripe.Customer.create(
@@ -50,27 +51,51 @@ def charge_transaction(transaction_uuid, stripe_token, email):
     db_transaction['receipt']['stripe_charge_id'] = stripe_charge.id
     db_transaction.save()
 
-@app.route('/core/payment/uiform/<transaction_uuid>', methods=['GET'])
+def generate_redirect(success, message=None):
+    args = {"success": success}
+    if message is not None:
+        args["message"] = message
+    url = "/payment/uistatus?" + urllib.urlencode(args)
+    return redirect(url, code=302)
+
+@app.route('/payment/uiform/<transaction_uuid>', methods=['GET'])
 def ui_form(transaction_uuid):
     try:
         transaction = get_chargeable_transaction(transaction_uuid)
-        amount = calculate_total(transaction['receipt'])
-        return render_template('payment.html', uuid=transaction_uuid, amount=amount, total=float(amount)/100.0,
-                items=transaction['receipt']['items'], notes=transaction['receipt']['notes'])
+        if "stripe_charge_id" in transaction['receipt']:
+            return generate_redirect(True)
+        else:
+            amount = calculate_total(transaction['receipt'])
+            return render_template('payment.html', uuid=transaction_uuid, amount=amount, total=float(amount)/100.0,
+                    items=transaction['receipt']['items'], notes=transaction['receipt']['notes'])
     except Exception as e:
         logging.exception(e)
-        return render_template('payment-error.html', message=str(e)), 500
+        return generate_redirect(False, str(e))
 
-@app.route('/core/payment/uicharge/<transaction_uuid>', methods=['POST'])
+@app.route('/payment/uicharge/<transaction_uuid>', methods=['POST'])
 def ui_charge(transaction_uuid):
     try:
         email = request.form['stripeEmail']
         token = request.form['stripeToken']
         charge_transaction(transaction_uuid, token, email)
-        return render_template('payment-success.html')
+        return generate_redirect(True)
     except stripe.error.CardError as e:
-        return render_template('payment-error.html', message=str(e)), 500
+        return generate_redirect(False, str(e))
+    except Exception as e:
+        logging.exception(e)
+        return generate_redirect(False, str(e))
+
+@app.route('/payment/uistatus/', methods=['GET'])
+def ui_status():
+    try:
+        success = request.args["success"] == "True"
+        if success:
+            return render_template('payment-success.html')
+        else:
+            message = request.args["message"]
+            return render_template('payment-error.html', message=message), 500
     except Exception as e:
         logging.exception(e)
         return render_template('payment-error.html', message=str(e)), 500
+
 
