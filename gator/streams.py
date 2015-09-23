a48f487cce2e9a6328682a6dd63aef24f7da9ae3
root <root@ip-172-31-39-136.us-west-2.compute.internal>
2015-09-23 01:13:15 +0000

    Changed a send to emit

--- gator/streams.py
+++ gator/streams.py
@@ -24,7 +24,7 @@ def transaction_change(transaction_uuid):
     transaction = gator.models.transactions.get_item(uuid=transaction_uuid, consistent=True)
 
     # Send the data back to the client
-    socketio.send(jsonpickle.encode(transaction._data), room=transaction_uuid)
+    socketio.emit(jsonpickle.encode(transaction._data), room=transaction_uuid)
 
     return jsonpickle.encode({"result": 0})
 
