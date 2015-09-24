aafa0ed822f3010696105ffc4e741e03a8477216
root <root@ip-172-31-39-136.us-west-2.compute.internal>
2015-09-24 05:56:09 +0000

    Basic twilio setup configured

--- gator/sms.py
+++ gator/sms.py
@@ -1,10 +1,18 @@
+from flask import request
+
 import gator.models
 import gator.common
 
 from gator import app
 
-@app.route("/sms/handle_sms")
-def handle_sms(data):
-    print data
+import jsonpickle
 
-    return '{"result": 0}'
+from twilio.rest import TwilioRestClient
+
+ACCOUNT_SID = "ACb5440a719947d5edf7d760155a39a768"
+AUTH_TOKEN = "dd9b4240a96556da1abb1e49646c73f3"
+
+@app.route("/sms/handle_sms", methods=["POST"])
+def handle_sms():
+    print request.values
+    return ""
