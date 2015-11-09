1e7da8e0ae703bcb0438415a1ac48811bd7fad6f
Austin Middleton <xuset1@gmail.com>
2015-11-09 15:30:51 -0600

    Added transaction stat method to utilities

--- gator/utilities.py
+++ gator/utilities.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python3.4
+#!/usr/bin/env python3
 """ The misc and utility file
 
 Put things in here that are scripts or tangential
 uses of the backend
 """
 
-from service import sms
+from gator.service import sms
+import copy
+import gator.models
 import sys
 
 def mass_text(body_fn, numbers_fn):
@@ -16,6 +18,38 @@ def mass_text(body_fn, numbers_fn):
         while cur_line != "":
             sms.send_msg(cur_line, body)
             cur_line = cur_file.readline()
+
+def retreive_transaction_info():
+    customers = {}
+    for t in gator.models.transactions.scan():
+        if t["customer_uuid"] not in customers:
+            customers[t["customer_uuid"]] = {
+                "transactions": []
+            }
+        transaction = copy.deepcopy(t._data)
+        del transaction["customer_uuid"]
+        del transaction["payment_url"]
+        if "messages" in transaction:
+            del transaction["messages"]
+        customers[t["customer_uuid"]]["transactions"].append(transaction)
+    customer_info = {}
+    for uuid, c in customers.items():
+        customer_info[uuid] = {}
+        customer_info[uuid]["transaction_count"] = len(c["transactions"])
+        paid = [t for t in c["transactions"] if "receipt" in t and "stripe_charge_id" in t["receipt"]]
+        customer_info[uuid]["transactions_paid"] = len(paid)
+        customer_info[uuid]["revenue"] = sum([int(t["receipt"]["total"]) for t in paid])
+    global_info = {}
+    global_info["total_customers"] = len(customer_info)
+    global_info["total_revenue"] = sum([c["revenue"] for c in customer_info.values()])
+    global_info["total_transactions"] = sum([c["transaction_count"] for c in customer_info.values()])
+    global_info["total_paid_transactions"] = sum([c["transactions_paid"] for c in customer_info.values()])
+    global_info["customers_1_transaction"] = len([c for c in customer_info.values() if c["transaction_count"] == 1])
+    global_info["customers_2_transaction"] = len([c for c in customer_info.values() if c["transaction_count"] == 2])
+    global_info["customers_3_transaction"] = len([c for c in customer_info.values() if c["transaction_count"] == 3])
+    global_info["customers_4_transaction"] = len([c for c in customer_info.values() if c["transaction_count"] == 4])
+    return (global_info, customer_info, customers)
+
 
 if __name__ == "__main__":
     method_map = {
