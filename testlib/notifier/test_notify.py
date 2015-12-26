b53af70694835685f23e37c691f4ea21d74f4f62
Austin Middleton <xuset1@gmail.com>
2015-12-25 23:22:21 -0600

    Updated notifier tests to use mocha
    Also added an "execApiClient" function that executes methods
    inside apiclient.py from within nodejs. This and mocha should
    make writing nodejs tests easier and better.
    
    Also replaced the explicit call to `POST /notify/broadcast/...`
    with the `POST /core/send_message`. This should test the newly
    implemented psuedo lambda server and dynamo streams.

--- testlib/notifier/test_notify.py
+++ testlib/notifier/test_notify.py
@@ -1,21 +1,12 @@
-import nose
 import unittest
 import subprocess
-import sys
-from gator import apiclient
 
 # This just setups the environment so nodejs can run it's test
 class NotifyTest(unittest.TestCase):
 
-    def setUp(self):
-        apiclient.clear_database()
-        cid = apiclient.create_customer("asdf", "sadfs", "15555555551", "1", "")["uuid"]
-        self.transaction_uuid = apiclient.create_transaction(cid, "sms")["uuid"]
-
     def test_end_2_end(self):
         # Nodejs should perform the actual test
-        # subprocess.check_call(["nodejs", "notifier/test.js", self.transaction_uuid], stdout=sys.stdout)
-        process = subprocess.Popen(["node", "test.js", self.transaction_uuid], stdout=subprocess.PIPE)
+        process = subprocess.Popen(["mocha", "test.js"], stdout=subprocess.PIPE)
         process.wait()
         print(process.stdout.read().decode("UTF-8").strip())
         self.assertEquals(process.returncode, 0)
