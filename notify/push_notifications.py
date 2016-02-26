3bfc752f2430ff7c65cf139629b3feede637db66
Austin Middleton <xuset1@gmail.com>
2016-02-26 12:35:56 -0600

    Revert "Added send_push_notification endpoint and updated the lambda"
    This reverts commit 58841e6112165bf7162335f9f81fb83190f9760d.

--- notify/push_notifications.py
+++ notify/push_notifications.py
@@ -1,7 +1,27 @@
-import request
+import boto.sns
+import json
 
-BASE_URL = "api.godelegateit.com"
-SEND_PUSH_NOTIFICATION_ENDPOINT = "%s/push/send_push_notification/" % BASE_URL
+SNS_ACCESS_KEY = None
+SNS_SECRET_KEY = None
+REGION = None
+PLATFORM_ENDPOINT_ARN = None
+
+def _load_config():
+    with open("config.json", "r") as f:
+        sns_config = json.loads(f.read())["sns"]
+        SNS_ACCESS_KEY = sns_config["access_key"]
+        SNS_SECRET_KEY = sns_config["secret_key"]
+        REGION = sns_config["region"]
+        PLATFORM_ENDPOINT_ARN = sns_config["platform_endpoint_arn"]
+
+_load_config()
+
+# Initialize the sns connection
+sns_conn = boto.sns.connect_to_region(
+    REGION,
+    aws_access_key_id=SNS_ACCESS_KEY,
+    aws_secret_access_key=SNS_SECRET_KEY,
+)
 
 # Handles dynamodb event stream request
 def handler(event, context):
@@ -19,9 +39,14 @@ def handler(event, context):
     if platform_type != "ios" or not from_delegator or new_last_message == old_last_message:
         return
 
-    # Request data
-    customer_uuid = new_item["customer_uuid"]["S"]
-    message = new_last_message["content"]["S"]
+    push_endpoint_arn = new_item["push_endpoint_arn"]["S"]
+    send_push_notifications(push_endpoint_arn, new_last_message["content"]["S"])
 
-    request.post("%s%s" % (SEND_PUSH_NOTIFICATION_ENDPOINT, customer_uuid),
-        data={"message": message})
+# Sends a push notification using boto's SNS
+def send_push_notifications(push_endpoint_arn, message):
+    publish_result = sns_conn.publish(
+        target_arn=push_endpoint_arn,
+        message=message,
+    )
+
+    print (publish_result)
