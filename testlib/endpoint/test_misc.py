04cbd0b2b402668f570acad4347161e091ab5e20
Austin Middleton <xuset1@gmail.com>
2016-04-22 21:48:28 -0500

    Clearing db before misc tests

--- testlib/endpoint/test_misc.py
+++ testlib/endpoint/test_misc.py
@@ -2,16 +2,22 @@ from gator import apiclient
 from endpoint.rest import RestTest
 
 class QuickOrdersTest(RestTest):
+    def setUp(self):
+        apiclient.clear_database()
     def test(self):
         rsp = apiclient.get_quickorders()
         self.assertResponse(0, rsp)
         self.assertNotEqual(0, len(rsp["quickorders"]))
 
 class SendGreetingTest(RestTest):
+    def setUp(self):
+        apiclient.clear_database()
     def test(self):
         self.assertResponse(0, apiclient.send_greeting("15555551234"))
 
 class HealthTest(RestTest):
+    def setUp(self):
+        apiclient.clear_database()
     def test(self):
         rsp = apiclient.get_health()
         self.assertResponse(0, rsp)
